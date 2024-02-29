5afa23983827056b84a34dbcc6984633cfc2715f
kaushik7749 <kaushikyadure4977@gmail.com>
2024-02-29 22:26:40 +0530

    ui design implemented
    ui design implemented

---
+++ frontend/src/components/messages/Message.jsx
@@ -0,0 +1,13 @@
+const Message = () => {
+  return (
+    <div className="chat chat-end">
+      <div className="chat-image avatar">
+        <div className="w-10 rounded-full">
+          <img alt="Tailwind CSS chat bubble component" />
+        </div>
+      </div>
+      <div className="{chat-bubble text-white bg-blue-500}">Hi what is up?</div>
+    </div>
+  );
+};
+export default Message;
