1b7dfffa125ce3a2a0c500595fbd4ba5ed9815a4
kaushik7749 <kaushikyadure4977@gmail.com>
2024-03-01 02:49:32 +0530

    refactored the code
    refactored the code

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -14,7 +14,7 @@ const MessageContainer = () => {
   }, [setSelectedConversation]);
 
   return (
-    <div className="md:min-w-[450px] flex flex-col">
+    <div className="md:min-w-[450px] flex flex-col bg-blue-100">
       {!selectedConversation ? (
         <NoChatSelected />
       ) : (
